92463380a993a5211f3d8e6314767cb3f70bef97
Mohit Kumar Yadav <79302660+Mohityadav-code@users.noreply.github.com>
2023-01-29 14:32:57 +0530

    Tags And Question Counter Added

--- quiz.js
+++ quiz.js
@@ -14,3 +14,18 @@ function nextbutton() {
     }
     functionsGroupForDom()
   }
+  
+  // tags
+var tagDomit = document.getElementById("tagbyid");
+domForTags = () => {
+  var temp = arrForTags[pointer];
+  tagDomit.innerText = temp;
+};
+
+// container 1
+// question no. counter
+function questionNoCounter(){
+  var questionNo= document.querySelector("#quehere1")
+  questionNo.innerHTML=`Question ${pointer+1}`
+  
+}
